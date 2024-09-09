7eb3329c77315e8571a9719a95f878c45eb3de9a
Rebecca Bettencourt <beckiergb@gmail.com>
2024-09-09 05:41:39 -0700

    Try an even crunchier bitmap

--- art/make_bitmap_64.py
+++ art/make_bitmap_64.py
@@ -4,17 +4,17 @@
 from PIL import Image
 
 # Open the image
-img = Image.open("tt09ball2c.png")
+img = Image.open("tt09ball2d.png")
 
 # Convert the image to grayscale
 img = img.convert("L")
-pix = bytearray(128 * 128 // 8)
+pix = bytearray(64 * 64 // 8)
 
 for y in range(img.height):
     for x in range(img.width):
         color = img.getpixel((x, y))
         if color < 128:
-            pix[y * 16 + x // 8] |= 1 << (x % 8)
+            pix[y * 8 + x // 8] |= 1 << (x % 8)
 
 module = []
 module.append("module bitmap_rom (")
@@ -23,14 +23,14 @@ module.append("    input wire [6:0] y,")
 module.append("    output wire pixel")
 module.append(");")
 module.append("")
-module.append("  reg [7:0] mem[2047:0];")
+module.append("  reg [7:0] mem[511:0];")
 module.append("  initial begin")
 for i, byte in enumerate(pix):
     module.append(f"    mem[{i}] = 8'h{byte:02x};")
 module.append("  end")
 module.append("")
-module.append("  wire [10:0] addr = {y[6:0], x[6:3]};")
-module.append("  assign pixel = mem[addr][x&7];")
+module.append("  wire [8:0] addr = {y[6:1], x[6:4]};")
+module.append("  assign pixel = mem[addr][x[3:1]];")
 module.append("")
 module.append("endmodule")
 module.append("")
